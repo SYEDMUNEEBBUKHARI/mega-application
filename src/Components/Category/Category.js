edb905a21d3f9133a5b9bb4b96a54cb45b0b4fe1
Muneeb Bukhari <mbukhari@TSG.local>
2020-12-18 17:41:22 +0500

    updated

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./category.css";
 import {FaHamburger} from "react-icons/fa";
+import {FiChevronDown,FiChevronUp} from "react-icons/fi"
 function Category(){
+let chevup=<FiChevronUp className="showchevron"/>;
+let chevdown=<FiChevronDown className="showchevron"/>;
+const[chevron,setChevron]=useState(false);
+const[show,setShow]=useState('show');
+  const handleCategory=()=>{
+    setChevron(!chevron);
+    if(chevron){
+      setShow("show")
+    }
+    else{
+      setShow("hide")
+    }
+}
+useEffect(()=>{},[chevron,show]);
+
 return(
 <>
 <div>
-  <ul className="menu">
-    <li className="vertical"><a  className="vertical-menu-item" href="#"><FaHamburger className="menu-icon"/>  BURGERS</a>
+<div className="categories text-center" onClick={handleCategory}><span className="category-position">Categories {chevron ? chevup : chevdown}</span></div>
+
+  <ul className={`menu ${show}`}>
+    <li className="vertical first"><a  className="vertical-menu-item" href="#"><FaHamburger className="menu-icon"/>  BURGERS</a>
 
       <div className="megadrop">
 
         <div className="col">
-          <h3>Title</h3>
+          <h3 className="title">Title</h3>
           <ul>
             <li><a href="#">Sub-menu 1</a>
             </li>
@@ -22,7 +40,7 @@ return(
           </ul>
         </div>
         <div className="col">
-          <h3>Title</h3>
+          <h3  className="title">Title</h3>
           <ul>
             <li><a href="#">Sub-menu 1</a>
             </li>
@@ -33,7 +51,7 @@ return(
           </ul>
         </div>
         <div className="col">
-          <h3>Title</h3>
+          <h3  className="title">Title</h3>
           <ul>
             <li><a href="#">Sub-menu 1</a>
             </li>
@@ -49,7 +67,7 @@ return(
     </li>
 
 
-    <li className="vertical"> <a  className="vertical-menu-item" href="#"><FaHamburger className="menu-icon" />  BURGERS</a>
+    <li className="vertical "> <a  className="vertical-menu-item" href="#"><FaHamburger className="menu-icon" />  BURGERS</a>
 
       <div className="megadrop">
 
